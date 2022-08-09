627e37389aacf0aa1ab7e9e325ed76e5c334133e
John Carlson <john@johns-mbp.attlocal.net>
2022-08-08 22:02:17 -0700

    added dynamic URLs

--- baseApp/views.py
+++ baseApp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me!'},
+    {'id':3, 'name':'Frontend Developer'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):
+    context = {'rooms': rooms} # key is variable used in for loop, pair is list
+    return render(request, 'baseApp/home.html', context) # passing context in makes it available in home.html
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    
+    context = {'room':room}
+
+    return render(request, 'baseApp/room.html', context)
