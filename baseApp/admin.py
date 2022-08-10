4bed7976280fa33a74d474e267e1b1314e5912b1
John Carlson <john@johns-mbp.attlocal.net>
2022-08-09 19:31:38 -0700

    Added DB

--- baseApp/admin.py
+++ baseApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Room, Topic, Message
+
+admin.site.register(Room) #Registers the Room so we can access in admin panel
+admin.site.register(Topic)
+admin.site.register(Message)
--- baseApp/views.py
+++ baseApp/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Design with me!'},
-    {'id':3, 'name':'Frontend Developer'},
-]
+from .models import Room
 
 def home(request):
+    rooms = Room.objects.all() #objects is used to query database table
     context = {'rooms': rooms} # key is variable used in for loop, pair is list
     return render(request, 'baseApp/home.html', context) # passing context in makes it available in home.html
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
     
+    room = Room.objects.get(id=pk)
     context = {'room':room}
 
     return render(request, 'baseApp/room.html', context)
